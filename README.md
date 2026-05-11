b65e45af7b9712486c0bb0e00dc43c9bdd7ad0b3
Aniket Dey <meaniket1025@gmail.com>
2026-05-11 23:51:05 +0530

    Refactor code structure for improved readability and maintainability

--- README.md
+++ README.md
@@ -1,56 +1,42 @@
-# Welcome to your Expo app 👋
+# React Native Signup Page
 
-This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
+A clean and modern React Native signup page built with Expo. This project provides a simple and attractive user interface for user registration, including responsive layout, form fields, and smooth styling.
 
-## Get started
+## Features
 
-1. Install dependencies
+- Sign-up screen layout with input fields for name, email, and password
+- Modern mobile design with a polished look and feel
+- Easy to customize for your own app branding
+- Built with Expo and TypeScript for fast development
+
+## Project Structure
+
+- `app/` – Expo app entry and layout files
+- `src/` – main source code for the signup screen and app UI
+- `assets/` – images and icons used in the app
+
+## Screenshot
+
+> Add a screenshot of the app here after you build and run it.
+
+![App Screenshot](/assets/images/projectScreen.jpeg)
+
+## Getting Started
+
+1. Install dependencies:
 
    ```bash
    npm install
    ```
 
-2. Start the app
+2. Start the Expo development server:
 
    ```bash
-   npx expo start
+   npm start
    ```
 
-In the output, you'll find options to open the app in a
+3. Open the app on your device or emulator using the Expo Go app.
 
-- [development build](https://docs.expo.dev/develop/development-builds/introduction/)
-- [Android emulator](https://docs.expo.dev/workflow/android-studio-emulator/)
-- [iOS simulator](https://docs.expo.dev/workflow/ios-simulator/)
-- [Expo Go](https://expo.dev/go), a limited sandbox for trying out app development with Expo
+## Customize
 
-You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
-
-## Get a fresh project
-
-When you're ready, run:
-
-```bash
-npm run reset-project
-```
-
-This command will move the starter code to the **app-example** directory and create a blank **app** directory where you can start developing.
-
-### Other setup steps
-
-- To set up ESLint for linting, run `npx expo lint`, or follow our guide on ["Using ESLint and Prettier"](https://docs.expo.dev/guides/using-eslint/)
-- If you'd like to set up unit testing, follow our guide on ["Unit Testing with Jest"](https://docs.expo.dev/develop/unit-testing/)
-- Learn more about the TypeScript setup in this template in our guide on ["Using TypeScript"](https://docs.expo.dev/guides/typescript/)
-
-## Learn more
-
-To learn more about developing your project with Expo, look at the following resources:
-
-- [Expo documentation](https://docs.expo.dev/): Learn fundamentals, or go into advanced topics with our [guides](https://docs.expo.dev/guides).
-- [Learn Expo tutorial](https://docs.expo.dev/tutorial/introduction/): Follow a step-by-step tutorial where you'll create a project that runs on Android, iOS, and the web.
-
-## Join the community
-
-Join our community of developers creating universal apps.
-
-- [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
-- [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+Update the layout, colors, and form fields in `src/` to match your app design and requirements.
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,283 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from "react"
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 
-const Home = () => {
-  const items = Array.from({length: 100}, (_, i) => `Item ${i+1}`)
+const index = () => {
+  const [isEmailFocused, setEmailFocused] = useState(false)
 
   return (
-    <ScrollView style={{flex:1, backgroundColor: '#f6f6f6'}} 
-      contentContainerStyle={{
-        padding: 16,
-        alignItems: "center"
-      }}
-    >
-      {
-        items.map((item) => (
-          <Text key={item}>{item}</Text>
-        ))
-      }
-    </ScrollView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F6F6F6" }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            paddingHorizontal: 20,
+          }}
+        >
+          {/* LOGO */}
+          <View
+            style={{
+              alignItems: "center",
+              marginBottom: 20,
+            }}
+          >
+            <Image source={require("@/assets/images/icon.png")} />
+          </View>
+
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 40,
+              fontWeight: "700",
+              color: "#1f1f1f",
+              marginBottom: 12,
+            }}
+          >
+            Sign In
+          </Text>
+
+          <Text
+            style={{
+              textAlign: "center",
+              color: "#111",
+              marginBottom: 40,
+              lineHeight: 22,
+              fontSize: 15,
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </Text>
+
+          {/* From  */}
+
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 600,
+              marginBottom: 12,
+              color: "#1f1f1f",
+            }}
+          >
+            Email Id
+          </Text>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              height: 58,
+              backgroundColor: "#fff",
+              borderRadius: 24,
+              paddingHorizontal: 20,
+              borderWidth: 2,
+              borderColor: isEmailFocused ? "#9BE000" : "#d9d9d9",
+              marginBottom: 24,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your email..."
+              placeholderTextColor="#7a7a7a"
+              keyboardType="email-address"
+              onFocus={() => setEmailFocused(true)}
+              onBlur={() => setEmailFocused(false)}
+              style={{
+                paddingLeft: 0,
+                flex: 1,
+                fontSize: 16,
+                color: "#1f1f1f",
+              }}
+            />
+          </View>
+
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 600,
+              marginBottom: 12,
+              color: "#1f1f1f",
+            }}
+          >
+            Password
+          </Text>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              height: 58,
+              backgroundColor: "#fff",
+              borderRadius: 24,
+              paddingHorizontal: 20,
+              borderWidth: 2,
+              borderColor: isEmailFocused ? "#9BE000" : "#d9d9d9",
+              marginBottom: 24,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your password..."
+              placeholderTextColor="#7a7a7a"
+              keyboardType="email-address"
+              onFocus={() => setEmailFocused(true)}
+              onBlur={() => setEmailFocused(false)}
+              style={{
+                paddingLeft: 0,
+                flex: 1,
+                fontSize: 16,
+                color: "#1f1f1f",
+              }}
+            />
+          </View>
+
+          <Pressable
+            style={({ pressed }) => ({
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 20,
+              backgroundColor: "#9BE000",
+              height: 50,
+              marginBottom: 42,
+              opacity: pressed ? 0.8 : 1,
+            })}
+          >
+            <Text
+              style={{
+                color: "#101010",
+                fontSize: 17,
+                fontFamily: "700",
+              }}
+            >
+              Sign In →
+            </Text>
+          </Pressable>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Pressable
+              style={({ pressed }) => ({
+                width: 68,
+                height: 68,
+                borderRadius: 24,
+                backgroundColor: "#fff",
+                justifyContent: "center",
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: "#e5e5e5",
+                marginHorizontal: 10,
+                opacity: pressed ? 0.7 : 1,
+              })}
+            >
+              <Image
+                source={require("@/assets/images/facebook.png")}
+                style={{
+                  width: 32,
+                  height: 32,
+                  resizeMode: "contain",
+                }}
+              />
+            </Pressable>
+
+            <Pressable
+              style={({ pressed }) => ({
+                width: 68,
+                height: 68,
+                borderRadius: 24,
+                backgroundColor: "#fff",
+                justifyContent: "center",
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: "#e5e5e5",
+                marginHorizontal: 10,
+                opacity: pressed ? 0.7 : 1,
+              })}
+            >
+              <Image
+                source={require("@/assets/images/instagram.png")}
+                style={{
+                  width: 32,
+                  height: 32,
+                  resizeMode: "contain",
+                }}
+              />
+            </Pressable>
+
+            <Pressable
+              style={({ pressed }) => ({
+                width: 68,
+                height: 68,
+                borderRadius: 24,
+                backgroundColor: "#fff",
+                justifyContent: "center",
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: "#e5e5e5",
+                marginHorizontal: 10,
+                opacity: pressed ? 0.7 : 1,
+              })}
+            >
+              <Image
+                source={require("@/assets/images/google.png")}
+                style={{
+                  width: 32,
+                  height: 32,
+                  resizeMode: "contain",
+                }}
+              />
+            </Pressable>
+          </View>
+
+          <Text
+            style={{
+              marginTop: 40,
+              textAlign: "center",
+              color: "#555",
+              fontSize: 15,
+            }}
+          >
+            Don't have an account?{" "}
+            <Text
+              style={{
+                color: "#9BE000",
+                fontWeight: "600",
+              }}
+            >
+              Sign Up
+            </Text>
+          </Text>
+
+          <Text
+            style={{
+              textAlign: "center",
+              marginTop: 14,
+              color: "#9be000",
+              textDecorationLine: "underline",
+              fontWeight: "600",
+              fontSize: 15
+            }}
+          >Forgot Password?</Text>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
-export default Home
+export default index
 
 const styles = StyleSheet.create({})
